d7d8b72e22c20ae7fd3066c26556ae975de70a06
Thiago Costa <thiagorpc@hotmail.com>
2025-11-10 14:20:12 -0300

    Atualização da config do Workflow

--- iac/main.tf
+++ iac/main.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "ecs_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
