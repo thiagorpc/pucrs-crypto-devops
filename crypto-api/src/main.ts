0057cfd49ca6621fc6c0968c3f5d8bba757ac38b
Thiago Costa <thiagorpc@hotmail.com>
2025-11-10 22:14:06 -0300

    Atualização da API CORS

--- crypto-api/src/main.ts
+++ crypto-api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // Habilita CORS
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
