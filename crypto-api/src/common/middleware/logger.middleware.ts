fe1e57b5a35e0748a4ab689f3d04ecbca39b676b
Thiago Costa <thiagorpc@hotmail.com>
2025-11-11 11:43:29 -0300

    Atualização do API Backend Logger

--- crypto-api/src/common/middleware/logger.middleware.ts
+++ crypto-api/src/common/middleware/logger.middleware.ts
@@ -9,10 +9,24 @@ export class LoggerMiddleware implements NestMiddleware {
     const { method, originalUrl } = req;
     const startTime = Date.now();
 
+    // Tenta obter o IP de origem real, verificando o cabeçalho X-Forwarded-For (comum em Load Balancers/API Gateways)
+    const ip = req.headers['x-forwarded-for'] || req.ip;
+
+    // Log inicial da requisição
+    const logMessage = `${method} ${originalUrl} | IP: ${ip}`;
+    this.logger.log(logMessage);
+
+    // Registra o evento de conclusão da resposta
     res.on('finish', () => {
       const { statusCode } = res;
       const duration = Date.now() - startTime;
-      this.logger.log(`${method} ${originalUrl} ${statusCode} - ${duration}ms`);
+      const contentLength = res.get('content-length');
+
+      // Log de resposta final com status e IP
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${duration}ms | IP: ${ip}`,
+        'HTTP Response',
+      );
     });
 
     next();
