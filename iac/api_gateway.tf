f44ce7b82614e3585fb3e027e8735c484ce5c8bf
Thiago Costa <thiagorpc@hotmail.com>
2025-11-11 09:55:34 -0300

    Atualização do API Gateway

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -2,6 +2,8 @@
 resource "aws_api_gateway_rest_api" "crypto_gateway" {
   name        = "crypto-api-gateway"
   description = "Gateway para o backend ECS/ALB"
+
+  cloudwatch_role_arn = aws_iam_role.apigw_cloudwatch_log_role.arn
 }
 
 # 2. Recurso Root (o path "/")
@@ -20,6 +22,7 @@ resource "aws_api_gateway_method" "proxy_method" {
 
 }
 
+# 4. Integração com ALB
 resource "aws_api_gateway_integration" "alb_integration" {
   rest_api_id             = aws_api_gateway_rest_api.crypto_gateway.id
   resource_id             = aws_api_gateway_resource.proxy.id
@@ -68,4 +71,51 @@ resource "aws_api_gateway_stage" "prod_stage" {
   deployment_id = aws_api_gateway_deployment.crypto_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.crypto_gateway.id
   stage_name    = "prod"
+}
+
+# 7. Política de Confiança: Permite que o serviço API Gateway assuma esta role
+data "aws_iam_policy_document" "apigw_log_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "apigw_cloudwatch_log_role" {
+  name               = "crypto-apigw-cloudwatch-role"
+  assume_role_policy = data.aws_iam_policy_document.apigw_log_assume_role.json
+}
+
+# 8. Política de Permissão: Permite gravar logs no CloudWatch
+resource "aws_iam_role_policy_attachment" "apigw_cloudwatch_attach" {
+  role       = aws_iam_role.apigw_cloudwatch_log_role.name
+  # Esta é a política gerenciada da AWS que dá as permissões exatas necessárias
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+# 9. Política de saída de Log
+resource "aws_api_gateway_stage" "prod_stage" {
+  deployment_id = aws_api_gateway_deployment.crypto_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.crypto_gateway.id
+  stage_name    = "prod"
+
+  # 🎯 NOVO: Habilita o Logging
+  access_log_settings {
+    # ARN do CloudWatch Log Group de destino (você pode criar um ou usar o default)
+    destination_arn = "arn:aws:logs:us-east-1:202533542500:log-group:/aws/apigateway/crypto-api-prod" 
+    
+    # Formato dos logs (Exemplo: Logs completos)
+    format = "$context.requestId $context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] \"$context.httpMethod $context.resourcePath $context.protocol\" $context.status $context.responseLength $context.integrationErrorMessage"
+  }
+
+  # Opcional: Habilita métricas detalhadas (Execution/Errors)
+  xray_tracing_enabled = true
+  
+  # 🎯 NOVO: Define os níveis de log (INFO, ERROR, OFF)
+  # log_level pode ser "INFO" para logs detalhados
+  # metrics_enabled = true
+  # cache_cluster_enabled = false 
 }
