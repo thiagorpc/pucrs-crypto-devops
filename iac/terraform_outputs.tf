c797c07dcb5b47d1cb140034cab14a0e445c7431
Thiago Costa <thiagorpc@hotmail.com>
2025-11-12 11:00:00 -0300

    Atualização

--- iac/terraform_outputs.tf
+++ iac/terraform_outputs.tf
@@ -1,24 +1,4 @@
-# ============================
-# File: ./iac/outputs.tf
-# ============================
 
-# DNS do Network Load Balancer (NLB)
-output "nlb_dns_name" {
-  value       = aws_lb.crypto_api_nlb.dns_name
-  description = "Endereço DNS público do NLB para acessar a API via API Gateway"
-}
-
-# ID do ECS Cluster
-output "ecs_cluster_id" {
-  value       = aws_ecs_cluster.crypto_cluster.id
-  description = "ID do ECS Cluster"
-}
-
-# URL do repositório ECR
-output "ecr_repository_url" {
-  value       = aws_ecr_repository.crypto_api_repo.repository_url
-  description = "URL do repositório ECR para armazenar imagens Docker"
-}
 
 # Security Group utilizado pelo ECS
 output "ecs_security_group_id" {
