783efac5f09bf0d102f2f12c3f4454e5e3d7f8dd
Thiago Costa <thiagorpc@hotmail.com>
2025-11-13 20:53:58 -0300

    Atualização

--- iac/flow/aws_ecs.tf
+++ iac/flow/aws_ecs.tf
@@ -228,6 +228,7 @@ resource "aws_ecs_service" "fargate" {
   name            = "${var.project_name}-api"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
+  dddddd
   desired_count   = 1
   launch_type     = "FARGATE"
 
