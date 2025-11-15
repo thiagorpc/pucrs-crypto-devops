b1bd17f5801b2ad331a4729e00471ed40bc18c03
Thiago Costa <thiagorpc@hotmail.com>
2025-11-14 22:00:01 -0300

    Atualização

--- crypto-api/test/app.e2e-spec.ts
+++ crypto-api/test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
@@ -14,12 +13,5 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
   });
 });
