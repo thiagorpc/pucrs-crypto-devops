19cae498919ab17dd9c7689fd659f97736cbf6e8
Thiago Costa <thiagorpc@hotmail.com>
2025-11-16 20:13:50 -0300

    Atualização da estrutura para PortMappings

--- iac/flow/aws_ecs.tf
+++ iac/flow/aws_ecs.tf
@@ -158,7 +158,7 @@ resource "aws_ecs_task_definition" "task" {
     image = "${aws_ecr_repository.image_repo.repository_url}:${var.image_tag}" 
     essential = true 
     portMappings = [ 
-      { containerPort = 3000, protocol = "tcp" } 
+      { containerPort="3000", protocol= "tcp"} 
     ]
 
     # Configuração de secrets para a task
